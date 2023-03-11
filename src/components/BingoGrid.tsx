62c459103ca77769760e3ce94c1950441679b625
Dimitry Lyomin <leminds@gmail.com>
2023-03-11 23:59:12 +0700

    Test pretty

--- src/components/BingoGrid.tsx
+++ src/components/BingoGrid.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import { GridDataItem } from '@/models/item'
+import React from "react"
+import {GridDataItem} from "@/models/item"
 
 type Props = {
   data: GridDataItem
 }
 
 export const BingoGrid: React.FC<Props> = ({data}) => {
-  return <>
-    <div className='mt-5 grid grid-cols-5 gap-4'>
-      {data.map(row => {
-        return row.map(cell => <div>{cell}</div>)
-      })}
-    </div>
-  </>
+  return (
+    <>
+      <div className="mt-5 grid grid-cols-5 gap-4">
+        {data.map((row) => {
+          return row.map((cell) => <div>{cell}</div>)
+        })}
+      </div>
+    </>
+  )
 }
